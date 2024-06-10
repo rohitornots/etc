9190963d3a52a9786ee7f926ca67f3f385fc319e
rohitornots <77712343+rohitornots@users.noreply.github.com>
2024-06-10 22:09:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggle = document.getElementById('theme-toggle');
+    const body = document.body;
+
+    themeToggle.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+    });
+});
